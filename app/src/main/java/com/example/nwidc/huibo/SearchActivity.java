11a0ef0eaa63493d1774664eba4e80bbefb69620
abona <ievi@live.com>
2017-06-25 11:50:43 +0800

    2017/6/24

--- app/src/main/java/com/example/nwidc/huibo/SearchActivity.java
+++ app/src/main/java/com/example/nwidc/huibo/SearchActivity.java
@@ -21,6 +21,7 @@ public class SearchActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
+
         //设置透明状态栏
         StatusbarUtils.enableTranslucentStatusbar(this);
 
