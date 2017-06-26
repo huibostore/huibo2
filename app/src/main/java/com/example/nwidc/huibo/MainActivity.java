33c5f3b538a0e5140e79edd64e118e77fd71d85e
abona <ievi@live.com>
2017-06-26 15:39:42 +0800

    20170626

--- app/src/main/java/com/example/nwidc/huibo/MainActivity.java
+++ app/src/main/java/com/example/nwidc/huibo/MainActivity.java
@@ -128,6 +128,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         if("Cart".equals(name)){
             txt_better.performClick();
+        }else if("Sort".equals(name)){
+            txt_settings.performClick();
         }
 
 
@@ -323,6 +325,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
+    //testFragment
+
+    public void onClickSort(View v){
+        txt_settings.performClick();
+    }
+
+    //购物车结账、
+    public void onClickOrder(View v ){
+        Intent intent = new Intent();
+        intent.setClass(MainActivity.this,Edit_orderActivity.class);
+        startActivity(intent);
+    }
+
     //降价
 
 //    public void onClickprice(View v){
@@ -342,78 +357,78 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     //摇一摇
-//    public void onClickShowPop(View v){
-//        TakePhotoPopWin takePhotoPopWin = new TakePhotoPopWin(this);
-//        //showAtLocation(View parent, int gravity, int x, int y)
-//        takePhotoPopWin.showAtLocation(findViewById(R.id.main_view), Gravity.CENTER, 0, 0);
-//    }
-//
-//    public class TakePhotoPopWin extends PopupWindow {
-//
-//        private Context mContext;
-//
-//        private View view;
-//
-//        private TextView btn_cancel;
-//
-//
-//        public TakePhotoPopWin(Context mContext) {
-//
-//            view = LayoutInflater.from(mContext).inflate(R.layout.shake_ani, null);
-//
-//                Typeface iconfont = Typeface.createFromAsset(getAssets(), "shake.ttf");
-//                TextView textview = (TextView)view.findViewById(R.id.shake);
-//                textview.setTypeface(iconfont);
-//
-//            // 设置按钮监听
-////        btn_pick_photo.setOnClickListener(itemsOnClick);
-////        btn_take_photo.setOnClickListener(itemsOnClick);
-//
-//            // 设置外部可点击
-//            this.setOutsideTouchable(true);
-//            // mMenuView添加OnTouchListener监听判断获取触屏位置如果在选择框外面则销毁弹出框
-//            this.view.setOnTouchListener(new View.OnTouchListener() {
-//
-//                public boolean onTouch(View v, MotionEvent event) {
-//
-//                    int height = view.findViewById(R.id.pop_layout).getTop();
-//
-//                    int y = (int) event.getY();
-//                    if (event.getAction() == MotionEvent.ACTION_UP) {
-//                        if (y < height) {
-//                            dismiss();
-//                        }
-//                    }
-//                    return true;
-//                }
-//            });
-//
-//
-//    /* 设置弹出窗口特征 */
-//            // 设置视图
-//            this.setContentView(this.view);
-//            // 设置弹出窗体的宽和高
-////            this.setHeight(RelativeLayout.LayoutParams.WRAP_CONTENT);
-////            this.setHeight(300);
-////            this.setWidth(300);
-//            this.setWidth(RelativeLayout.LayoutParams.WRAP_CONTENT);
+    public void onClickShowPop(View v){
+        TakePhotoPopWin takePhotoPopWin = new TakePhotoPopWin(this);
+        //showAtLocation(View parent, int gravity, int x, int y)
+        takePhotoPopWin.showAtLocation(findViewById(R.id.main_view), Gravity.CENTER, 0, 0);
+    }
+
+    public class TakePhotoPopWin extends PopupWindow {
+
+        private Context mContext;
+
+        private View view;
+
+        private TextView btn_cancel;
+
+
+        public TakePhotoPopWin(Context mContext) {
+
+            view = LayoutInflater.from(mContext).inflate(R.layout.shake_ani, null);
+
+                Typeface iconfont = Typeface.createFromAsset(getAssets(), "shake.ttf");
+                TextView textview = (TextView)view.findViewById(R.id.shake);
+                textview.setTypeface(iconfont);
+
+            // 设置按钮监听
+//        btn_pick_photo.setOnClickListener(itemsOnClick);
+//        btn_take_photo.setOnClickListener(itemsOnClick);
+
+            // 设置外部可点击
+            this.setOutsideTouchable(true);
+            // mMenuView添加OnTouchListener监听判断获取触屏位置如果在选择框外面则销毁弹出框
+            this.view.setOnTouchListener(new View.OnTouchListener() {
+
+                public boolean onTouch(View v, MotionEvent event) {
+
+                    int height = view.findViewById(R.id.pop_layout).getTop();
+
+                    int y = (int) event.getY();
+                    if (event.getAction() == MotionEvent.ACTION_UP) {
+                        if (y < height) {
+                            dismiss();
+                        }
+                    }
+                    return true;
+                }
+            });
+
+
+    /* 设置弹出窗口特征 */
+            // 设置视图
+            this.setContentView(this.view);
+            // 设置弹出窗体的宽和高
 //            this.setHeight(RelativeLayout.LayoutParams.WRAP_CONTENT);
-//
-//            // 设置弹出窗体可点击
-//            this.setFocusable(true);
-//
-//            // 实例化一个ColorDrawable颜色为半透明
-//            ColorDrawable dw = new ColorDrawable(0x00000000);
-//            // 设置弹出窗体的背景
-//            this.setBackgroundDrawable(dw);
-//
-//            // 设置弹出窗体显示时的动画，从底部向上弹出
-//            this.setAnimationStyle(R.style.take_photo_anim);
-//
-//        }
-//
-//    }
-//
+//            this.setHeight(300);
+//            this.setWidth(300);
+            this.setWidth(RelativeLayout.LayoutParams.WRAP_CONTENT);
+            this.setHeight(RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+            // 设置弹出窗体可点击
+            this.setFocusable(true);
+
+            // 实例化一个ColorDrawable颜色为半透明
+            ColorDrawable dw = new ColorDrawable(0x00000000);
+            // 设置弹出窗体的背景
+            this.setBackgroundDrawable(dw);
+
+            // 设置弹出窗体显示时的动画，从底部向上弹出
+            this.setAnimationStyle(R.style.take_photo_anim);
+
+        }
+
+    }
+
 
         @Override
     public void onClick(View v) {
