f0eec8901325e56487587768c2b72c5ae460be3b
abona <ievi@live.com>
2017-07-12 18:10:12 +0800

    201707012

--- app/src/main/java/com/example/nwidc/huibo/activity/BookingActivity.java
+++ app/src/main/java/com/example/nwidc/huibo/activity/BookingActivity.java
@@ -1,198 +1,314 @@
 package com.example.nwidc.huibo.activity;
 
-import android.os.AsyncTask;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.CoordinatorLayout;
-import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.StaggeredGridLayoutManager;
-import android.support.v7.widget.Toolbar;
-import android.support.v7.widget.helper.ItemTouchHelper;
-import android.text.TextUtils;
-import android.util.TypedValue;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
-import com.example.nwidc.huibo.Adapter.GridAdapter;
-import com.example.nwidc.huibo.Meizi;
-import com.example.nwidc.huibo.MyOkhttp;
+import com.alibaba.android.vlayout.DelegateAdapter;
+import com.alibaba.android.vlayout.VirtualLayoutManager;
+import com.alibaba.android.vlayout.layout.ColumnLayoutHelper;
+import com.alibaba.android.vlayout.layout.FixLayoutHelper;
+import com.alibaba.android.vlayout.layout.FloatLayoutHelper;
+import com.alibaba.android.vlayout.layout.GridLayoutHelper;
+import com.alibaba.android.vlayout.layout.LinearLayoutHelper;
+import com.alibaba.android.vlayout.layout.OnePlusNLayoutHelper;
+import com.alibaba.android.vlayout.layout.OnePlusNLayoutHelperEx;
+import com.alibaba.android.vlayout.layout.ScrollFixLayoutHelper;
+import com.alibaba.android.vlayout.layout.SingleLayoutHelper;
+import com.alibaba.android.vlayout.layout.StaggeredGridLayoutHelper;
+import com.alibaba.android.vlayout.layout.StickyLayoutHelper;
+import com.example.nwidc.huibo.Adapter.HomeContextAdapter;
 import com.example.nwidc.huibo.R;
-import com.example.nwidc.huibo.SnackbarUtil;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
+import com.example.nwidc.huibo.View.ItemClick;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 
-public class BookingActivity extends AppCompatActivity {
-
-    private static RecyclerView recyclerview;
-    private CoordinatorLayout coordinatorLayout;
-    private GridAdapter mAdapter;
-    private List<Meizi> meizis;
-    private StaggeredGridLayoutManager mLayoutManager;
-    private int lastVisibleItem ;
-    private int page=1;
-    private ItemTouchHelper itemTouchHelper;
-    private SwipeRefreshLayout swipeRefreshLayout;
+public class BookingActivity extends AppCompatActivity implements ItemClick {
+    RecyclerView recyclerView;
+    HomeContextAdapter Adapter_linearLayout,Adapter_GridLayout,Adapter_FixLayout,Adapter_ScrollFixLayout
+            ,Adapter_FloatLayout,Adapter_ColumnLayout,Adapter_SingleLayout,Adapter_onePlusNLayout,
+            Adapter_StickyLayout,Adapter_StaggeredGridLayout;
+    private ArrayList<HashMap<String, Object>> listItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_staggerde);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.main_activity);
 
-        initView();
-        setListener();
+        /**
+         * 步骤1：创建RecyclerView & VirtualLayoutManager 对象并进行绑定
+         * */
+        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        // 创建RecyclerView对象
 
-        new GetData().execute("http://gank.io/api/data/福利/10/1");
+        VirtualLayoutManager layoutManager = new VirtualLayoutManager(this);
+        // 创建VirtualLayoutManager对象
+        // 同时内部会创建一个LayoutHelperFinder对象，用来后续的LayoutHelper查找
+
+        recyclerView.setLayoutManager(layoutManager);
+        // 将VirtualLayoutManager绑定到recyclerView
+
+        /**
+         * 步骤2：设置组件复用回收池
+         * */
+        RecyclerView.RecycledViewPool viewPool = new RecyclerView.RecycledViewPool();
+        recyclerView.setRecycledViewPool(viewPool);
+        viewPool.setMaxRecycledViews(0, 10);
+
+
+        /**
+         * 步骤3:设置需要存放的数据
+         * */
+        listItem = new ArrayList<HashMap<String, Object>>();
+        for (int i = 0; i < 100; i++) {
+            HashMap<String, Object> map = new HashMap<String, Object>();
+            map.put("ItemTitle", "第" + i + "行");
+            map.put("ItemImage", R.mipmap.ic_launcher);
+            listItem.add(map);
+
+        }
+
+
+
+
+
+        /**
+         * 步骤4:根据数据列表,创建对应的LayoutHelper
+         * */
+
+
+        /**
+         设置线性布局
+         */
+        LinearLayoutHelper linearLayoutHelper = new LinearLayoutHelper();
+        // 创建对应的LayoutHelper对象
+
+        // 公共属性
+        linearLayoutHelper.setItemCount(1);// 设置布局里Item个数
+         linearLayoutHelper.setBgColor(Color.GRAY);// 设置背景颜色
+        linearLayoutHelper.setAspectRatio(0);// 设置设置布局内每行布局的宽与高的比
+
+        // linearLayoutHelper特有属性
+        linearLayoutHelper.setDividerHeight(10);
+        // 设置间隔高度
+        // 设置的间隔会与RecyclerView的addItemDecoration（）添加的间隔叠加.
+
+        linearLayoutHelper.setMarginBottom(100);
+        // 设置布局底部与下个布局的间隔
+
+
+        // 创建自定义的Adapter对象 & 绑定数据 & 绑定对应的LayoutHelper进行布局绘制
+        Adapter_linearLayout  = new HomeContextAdapter(this, linearLayoutHelper, 1, listItem) {
+            @Override
+            public void onBindViewHolder(MainViewHolder holder, int position) {
+                super.onBindViewHolder(holder, position);
+                if (position == 0) {
+                    holder.image.setImageResource(R.drawable.fruit);
+                }
+            }
+        };
+
+        Adapter_linearLayout.setOnItemClickListener(this);
+        // 设置每个Item的点击事件
+
+
+
+
+        /**
+         设置Grid布局
+         */
+        GridLayoutHelper gridLayoutHelper = new GridLayoutHelper(5);
+        // 在构造函数设置每行的网格个数
+
+        // 公共属性
+        gridLayoutHelper.setItemCount(10);// 设置布局里Item个数
+        gridLayoutHelper.setMarginBottom(100);
+        // 设置布局底部与下个布局的间隔
+
+        // gridLayoutHelper特有属性
+        gridLayoutHelper.setAutoExpand(false);//是否自动填充空白区域
+        gridLayoutHelper.setSpanCount(5);// 设置每行多少个网格
+
+        Adapter_GridLayout  = new HomeContextAdapter(this, gridLayoutHelper,10, listItem) {
+            // 设置需要展示的数据总数,此处设置是8,即展示总数是8个,然后每行是4个(上面设置的)
+            // 为了展示效果,通过重写onBindViewHolder()将布局的第一个数据设置为gridLayoutHelper
+            @Override
+            public void onBindViewHolder(MainViewHolder holder, int position) {
+                super.onBindViewHolder(holder, position);
+                // 为了展示效果,将布局里不同位置的Item进行背景颜色设置
+                if (position < 2) {
+                    holder.itemView.setBackgroundColor(0x66cc0000 + (position - 6) * 128);
+                } else if (position % 2 == 0) {
+                    holder.itemView.setBackgroundColor(0xaa22ff22);
+                } else {
+                    holder.itemView.setBackgroundColor(0xccff22ff);
+                }
+
+
+
+                if (position == 0) {
+                    holder.Text.setText("Grid");
+                }
+            }
+        };
+
+        Adapter_GridLayout.setOnItemClickListener(this);
+        // 设置每个Item的点击事件
+
+
+        /**
+         设置栏格布局
+         */
+        ColumnLayoutHelper columnLayoutHelper = new ColumnLayoutHelper();
+        // 创建对象
+
+        // 公共属性
+        columnLayoutHelper.setItemCount(3);// 设置布局里Item个数
+        columnLayoutHelper.setBgColor(Color.GRAY);// 设置背景颜色
+        columnLayoutHelper.setAspectRatio(6);// 设置设置布局内每行布局的宽与高的比
+        columnLayoutHelper.setMarginBottom(100);
+        // 设置布局底部与下个布局的间隔
+
+        // columnLayoutHelper特有属性
+        columnLayoutHelper.setWeights(new float[]{30, 40, 30});// 设置该行每个Item占该行总宽度的比例
+
+        Adapter_ColumnLayout = new HomeContextAdapter(this, columnLayoutHelper,3, listItem) {
+            // 设置需要展示的数据总数,此处设置是3
+            // 为了展示效果,通过重写onBindViewHolder()将布局的第一个数据设置为Column
+            @Override
+            public void onBindViewHolder(MainViewHolder holder, int position) {
+                super.onBindViewHolder(holder, position);
+                if (position == 0) {
+                    holder.Text.setText("Column");
+                }
+            }
+        };
+
+        Adapter_ColumnLayout.setOnItemClickListener(this);
+        // 设置每个Item的点击事件
+
+
+
+        /**
+         设置1拖N布局
+         */
+        OnePlusNLayoutHelperEx onePlusNLayoutHelper = new OnePlusNLayoutHelperEx();
+        // 在构造函数里传入显示的Item数
+        // 最多是1拖4,即5个
+
+        // 公共属性
+//        onePlusNLayoutHelper.setPadding(20, 20, 20, 20);// 设置LayoutHelper的子元素相对LayoutHelper边缘的距离
+//        onePlusNLayoutHelper.setMargin(20, 20, 20, 20);// 设置LayoutHelper边缘相对父控件（即RecyclerView）的距离
+//        onePlusNLayoutHelper.setBgColor(0xff876384);// 设置背景颜色
+        onePlusNLayoutHelper.setMargin(0, 10, 0, 10);
+//        onePlusNLayoutHelper.setColWeights(new float[]{40f, 30f, 30f, 30f, 30f});
+
+
+        Adapter_onePlusNLayout = new HomeContextAdapter(this, onePlusNLayoutHelper,5, listItem) {
+
+        };
+
+        Adapter_onePlusNLayout.setOnItemClickListener(this);
+        // 设置每个Item的点击事件
+
+        /**
+         设置瀑布流布局
+         */
+
+        StaggeredGridLayoutHelper staggeredGridLayoutHelper = new StaggeredGridLayoutHelper();
+        // 创建对象
+
+        // 公有属性
+        staggeredGridLayoutHelper.setItemCount(20);// 设置布局里Item个数
+        staggeredGridLayoutHelper.setBgColor(Color.GRAY);// 设置背景颜色
+        staggeredGridLayoutHelper.setAspectRatio(3);// 设置设置布局内每行布局的宽与高的比
+
+        // 特有属性
+        staggeredGridLayoutHelper.setLane(2);// 设置控制瀑布流每行的Item数
+        staggeredGridLayoutHelper.setHGap(20);// 设置子元素之间的水平间距
+        staggeredGridLayoutHelper.setVGap(15);// 设置子元素之间的垂直间距
+
+        Adapter_StaggeredGridLayout = new HomeContextAdapter(this, staggeredGridLayoutHelper,20, listItem) {
+            // 设置需要展示的数据总数,此处设置是20
+
+            // 通过重写onBindViewHolder()设置更加丰富的布局
+            @Override
+            public void onBindViewHolder(MainViewHolder holder, int position) {
+                super.onBindViewHolder(holder, position);
+                ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,150 +position % 5 * 20);
+                holder.itemView.setLayoutParams(layoutParams);
+
+                // 为了展示效果,设置不同位置的背景色
+                if (position > 10) {
+                    holder.itemView.setBackgroundColor(0x66cc0000 + (position - 6) * 128);
+                } else if (position % 2 == 0) {
+                    holder.itemView.setBackgroundColor(0xaa22ff22);
+                } else {
+                    holder.itemView.setBackgroundColor(0xccff22ff);
+                }
+
+                // 为了展示效果,通过将布局的第一个数据设置为staggeredGrid
+                if (position == 0) {
+                    holder.Text.setText("staggeredGrid");
+
+
+                }
+            }
+        };
+
+        Adapter_StaggeredGridLayout.setOnItemClickListener(this);
+        // 设置每个Item的点击事件
+
+
+        /**
+         *步骤5:将生成的LayoutHelper 交给Adapter，并绑定到RecyclerView 对象
+         **/
+
+        // 1. 设置Adapter列表（同时也是设置LayoutHelper列表）
+        List<DelegateAdapter.Adapter> adapters = new LinkedList<>();
+
+        // 2. 将上述创建的Adapter对象放入到DelegateAdapter.Adapter列表里
+        adapters.add(Adapter_linearLayout) ;
+
+        adapters.add(Adapter_GridLayout) ;
+
+        adapters.add(Adapter_ColumnLayout) ;
+
+        adapters.add(Adapter_onePlusNLayout) ;
+        adapters.add(Adapter_StaggeredGridLayout) ;
+
+
+        //
+        // 3. 创建DelegateAdapter对象 & 将layoutManager绑定到DelegateAdapter
+        DelegateAdapter delegateAdapter = new DelegateAdapter(layoutManager);
+
+        // 4. 将DelegateAdapter.Adapter列表绑定到DelegateAdapter
+        delegateAdapter.setAdapters(adapters);
+
+        // 5. 将delegateAdapter绑定到recyclerView
+        recyclerView.setAdapter(delegateAdapter);
+
+
+
+
 
     }
 
-    private void initView(){
-        coordinatorLayout=(CoordinatorLayout)findViewById(R.id.staggered_coordinatorLayout);
-
-        recyclerview=(RecyclerView)findViewById(R.id.staggered_recycler);
-        mLayoutManager=new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
-        recyclerview.setLayoutManager(mLayoutManager);
-
-        swipeRefreshLayout=(SwipeRefreshLayout) findViewById(R.id.staggered_swipe_refresh) ;
-        swipeRefreshLayout.setColorSchemeResources(R.color.colorPrimary,R.color.colorPrimaryDark,R.color.colorAccent);
-        swipeRefreshLayout.setProgressViewOffset(false, 0,  (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 24, getResources().getDisplayMetrics()));
-
-    }
-
-    private void setListener(){
-        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                page=1;
-                new GetData().execute("http://gank.io/api/data/福利/10/1");
-            }
-        });
-
-        itemTouchHelper=new ItemTouchHelper(new ItemTouchHelper.Callback() {
-            @Override
-            public int getMovementFlags(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
-                int dragFlags=0;
-                if(recyclerView.getLayoutManager() instanceof StaggeredGridLayoutManager){
-                    dragFlags=ItemTouchHelper.UP|ItemTouchHelper.DOWN|ItemTouchHelper.LEFT|ItemTouchHelper.RIGHT;
-                }
-                return makeMovementFlags(dragFlags,0);
-            }
-
-            @Override
-            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
-                int from=viewHolder.getAdapterPosition();
-                int to=target.getAdapterPosition();
-                Meizi moveItem=meizis.get(from);
-                meizis.remove(from);
-                meizis.add(to,moveItem);
-                mAdapter.notifyItemMoved(from,to);
-                return true;
-            }
-
-            @Override
-            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
-
-            }
-
-            @Override
-            public boolean isLongPressDragEnabled() {
-                return false;
-            }
-        });
-
-        recyclerview.addOnScrollListener(new RecyclerView.OnScrollListener() {
-            @Override
-            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
-                super.onScrollStateChanged(recyclerView, newState);
-//                0：当前屏幕停止滚动；1时：屏幕在滚动 且 用户仍在触碰或手指还在屏幕上；2时：随用户的操作，屏幕上产生的惯性滑动；
-                if (newState == RecyclerView.SCROLL_STATE_IDLE
-                        && lastVisibleItem +2>=mLayoutManager.getItemCount()) {
-                    new GetData().execute("http://gank.io/api/data/福利/10/"+(++page));
-                }
-            }
-
-            @Override
-            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
-                super.onScrolled(recyclerView, dx, dy);
-                int[] positions= mLayoutManager.findLastVisibleItemPositions(null);
-                lastVisibleItem = Math.max(positions[0],positions[1]);
-            }
-        });
-    }
-
-    private class GetData extends AsyncTask<String, Integer, String> {
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-
-            swipeRefreshLayout.setRefreshing(true);
-        }
-
-        @Override
-        protected String doInBackground(String... params) {
-
-            return MyOkhttp.get(params[0]);
-        }
-
-        protected void onPostExecute(String result) {
-            super.onPostExecute(result);
-            if(!TextUtils.isEmpty(result)){
-
-                JSONObject jsonObject;
-                Gson gson=new Gson();
-                String jsonData=null;
-
-                try {
-                    jsonObject = new JSONObject(result);
-                    jsonData = jsonObject.getString("results");
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-                if(meizis==null||meizis.size()==0){
-                    meizis= gson.fromJson(jsonData, new TypeToken<List<Meizi>>() {}.getType());
-                    Meizi pages=new Meizi();
-                    pages.setPage(page);
-                    meizis.add(pages);
-                }else{
-                    List<Meizi> more= gson.fromJson(jsonData, new TypeToken<List<Meizi>>() {}.getType());
-                    meizis.addAll(more);
-                    Meizi pages=new Meizi();
-                    pages.setPage(page);
-                    meizis.add(pages);
-                }
-
-                if(mAdapter==null){
-                    recyclerview.setAdapter(mAdapter = new GridAdapter(BookingActivity.this,meizis));
-
-                    //设置点击监听
-                    mAdapter.setOnItemClickListener(new GridAdapter.OnRecyclerViewItemClickListener() {
-                        @Override
-                        public void onItemClick(View view) {
-                            int position=recyclerview.getChildAdapterPosition(view);
-                            SnackbarUtil.ShortSnackbar(coordinatorLayout,"点击第"+position+"个",SnackbarUtil.Info).show();
-                        }
-
-                        @Override
-                        public void onItemLongClick(View view) {
-                            itemTouchHelper.startDrag(recyclerview.getChildViewHolder(view));
-                        }
-                    });
-
-                    itemTouchHelper.attachToRecyclerView(recyclerview);
-                }else{
-                    mAdapter.notifyDataSetChanged();
-                }
-            }
-            swipeRefreshLayout.setRefreshing(false);
-        }
+    /**
+     *步骤7:实现Item点击事件
+     **/
+    // 点击事件的回调函数
+    @Override
+    public void onItemClick(View view, int postion) {
+        System.out.println("点击了第"+postion+"行");
+        Toast.makeText(this, (String) listItem.get(postion).get("ItemTitle"), Toast.LENGTH_SHORT).show();
     }
 
 }
-
