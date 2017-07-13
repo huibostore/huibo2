2957fd9a8e16936254f9f48d671df6880996306d
stunievi <ievi@live.com>
2017-07-13 14:13:54 +0800

    New (#21)
    * 2017-6-14
    
    * Create README.md
    
    * 20170614
    
    * 20170614
    
    * 20170615
    
    * 2017/06/16
    
    * 20170616
    
    * 20170616
    
    * 20170616
    
    * 2017/06/19
    
    * 20170619
    
    * 20170620
    
    * 20170621
    
    * 20170621
    
    * 20170621
    
    * 20170621
    
    * 20170622
    
    * 20170622
    
    * 20170623
    
    * 20170623
    
    * 20170623
    
    * 20170623
    
    * 20170623
    
    * 20170623
    
    * 20170623
    
    * 2017/6/24
    
    * 2017/6/24
    
    * 2017/6/24
    
    * 2017/6/25
    
    * 20170626
    
    * 20170626
    
    * 20170626
    
    * 20170626
    
    * 20170707
    
    * 20170705
    
    * 20170706
    
    * 20170706
    
    * 20170706
    
    * 20170706
    
    * 20170706
    
    * 201707011
    
    * 201707011
    
    * 201707012
    
    * 201707013

---
+++ app/src/main/java/com/example/nwidc/huibo/Meizi.java
@@ -0,0 +1,28 @@
+package com.example.nwidc.huibo;
+
+/**
+ * Created by hello on 2017/7/11.
+ */
+
+public class Meizi {
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    private String url;
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    private int page;
+}
+
